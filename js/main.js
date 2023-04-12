12fe915640070379680ae873b334f063c562f1f4
Maria Flores <mariafd1212@gmail.com>
2023-04-11 23:17:07 -0300

    actualizacion js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-//Bienvenida
+/* //Bienvenida
 let nombreIngresado = prompt("Ingrese su nombre ");
 alert("Bienvenido/a " + nombreIngresado);
 
@@ -76,3 +76,4 @@ function calculoIva(idioma) {
 }
 //Llamando el resultado por la consola
 console.log(calculoIva(idioma));
+ */
